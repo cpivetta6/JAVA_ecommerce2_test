53eba5c968bd3a474bb1ba0bba4eb99584111de0
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-17 09:25:09 +0100

    Product and Category

--- src/main/java/com/caiopivetta6/domain/Category.java
+++ src/main/java/com/caiopivetta6/domain/Category.java
@@ -1,5 +1,67 @@
 package com.caiopivetta6.domain;
 
-public class Category {
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
 
+import com.caiopivetta6.domain.enums.CategoryName;
+
+public class Category implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private CategoryName categoryName;
+	
+	private List<Product> products;
+
+	public Category(Integer id, CategoryName categoryName) {
+		super();
+		this.id = id;
+		this.categoryName = categoryName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public CategoryName getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(CategoryName categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
 }
--- src/main/java/com/caiopivetta6/domain/Product.java
+++ src/main/java/com/caiopivetta6/domain/Product.java
@@ -1,7 +1,87 @@
 package com.caiopivetta6.domain;
 
-public class Product {
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
 
+public class Product implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String description;
+	private Double price;
+	
+	private List<Category> categories;
+
+	public Product(Integer id, String name, String description, Double price, List<Category> categories) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.categories = categories;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 }
